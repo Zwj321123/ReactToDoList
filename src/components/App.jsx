5e7c665e5b8d444bf6695c28ae8786bf9af560ec
Wenjun Zeng <wz165@duke.edu>
2023-03-25 18:03:18 -0400

    spearate inputarea into new component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,9 @@
 import React, {useState} from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-    const [inputText, setInputText] = useState("");
     const [items, setItems] = useState([]);
-
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-    }
 
     //when the item is clicked, it should be deleted from the list in parent component
     function deleteItem(id) {
@@ -19,25 +14,20 @@ function App() {
         });
     }
 
-    function addItem() {
+    function addItem(inputText) {
         setItems(prevItems => {
             //spread operator
             //prevItems.push(inputText);
             return [...prevItems, inputText];
         });
-        setInputText("");
+
     }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+        <InputArea onAdded={addItem}/>
       <div>
         <ul>
             {items.map((item, index) => (
